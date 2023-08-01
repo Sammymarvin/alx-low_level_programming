415d88edab81ca680048f6afdcdb1b4ed4991040
Sammymarvin <marvinsammy0@gmail.com>
2023-08-01 06:07:37 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/*
+ * deletes node linked list given an index
+ * will return 1(if successful)2(if fails)
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
+
+
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+
+	return (1);
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/*
+ *  pop_listint - deletes the head node
+ *
+ *  Return:message that the data was deleted
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
